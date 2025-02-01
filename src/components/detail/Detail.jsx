b0f67cce7c85cf86129e2491c44ce9b30e721352
Ben Alkureishi <benalkureishi@hotmail.co.uk>
2025-02-01 16:59:10 +0000

    hopefully a final fix for the problem.

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -37,50 +37,55 @@ export const Detail = ({ className }) => {
 	}
 
 	return (
-		<div className={`detail ${className}`}>
-			<div className='current-user'>
-				{currentUser?.avatar ? (
-					<img src={currentUser?.avatar} className='avatar'/>
-				) : (
-					<div className='user-icon'>
-						<User className='avatar' />
+		<div className={`detail-container ${className}`}>
+			<div className='detail'>
+				<div className='current-user'>
+					{currentUser?.avatar ? (
+						<img
+							src={currentUser?.avatar}
+							className='avatar'
+						/>
+					) : (
+						<div className='user-icon'>
+							<User className='avatar' />
+						</div>
+					)}
+					<h2>{currentUser?.username}</h2>
+					<p>{currentUser?.status}</p>
+					<div className='close'>
+						<X
+							onClick={() => {
+								toggleDetail()
+							}}
+							className='icon'
+						/>
 					</div>
-				)}
-				<h2>{currentUser?.username}</h2>
-				<p>{currentUser?.status}</p>
-				<div className='close'>
-					<X
-						onClick={() => {
-							toggleDetail()
-						}}
-						className='icon'
-					/>
 				</div>
-			</div>
-			<div className='info'>
-				<Dropdown title='Shared Files'>
-					<SharedFiles
-						files={images}
-						isLoading={loading}
-						error={error}
-					/>
-				</Dropdown>
-				<Dropdown title='Uploaded Files'>
-					<UploadedFiles />
-				</Dropdown>
-			</div>
-			<div className='buttons'>
-				<button
-					className='block'
-					onClick={handleBlock}
-					disabled={isCurrentUserBlocked}
-				>
-					{isCurrentUserBlocked
-						? 'You are blocked'
-						: isReceiverBlocked
-						? 'User Blocked'
-						: 'Block User'}{' '}
-				</button>
+				<div className='info'>
+					<Dropdown title='Shared Files'>
+						<SharedFiles
+							files={images}
+							isLoading={loading}
+							error={error}
+						/>
+					</Dropdown>
+					<Dropdown title='Uploaded Files'>
+						<UploadedFiles />
+					</Dropdown>
+				</div>
+				<div className='buttons'>
+					<button
+						className='block'
+						onClick={handleBlock}
+						disabled={isCurrentUserBlocked}
+					>
+						{isCurrentUserBlocked
+							? 'You are blocked'
+							: isReceiverBlocked
+							? 'User Blocked'
+							: 'Block User'}{' '}
+					</button>
+				</div>
 			</div>
 		</div>
 	)
